cf10eee9d3eca6ddb9546e508d66e588f5ce2282
diguage <leejun119@gmail.com>
2012-12-26 18:27:12 +0800

    增加第一个练习，学习了输出和注释

---
+++ ex01.rb
@@ -0,0 +1,7 @@
+puts "Hello world!"
+#puts "Hello again"  # 从这行看出来，#是当行注释
+#puts "I like typing this."
+puts "This is fun."
+puts 'Yay! Printing.'
+puts "I'd much rather you 'not'."
+puts 'I "said" do not touch this.'
