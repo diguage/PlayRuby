3014fa780d115eebe0b67c39868586694e2ba8fc
diguage <leejun119@gmail.com>
2012-12-27 18:08:11 +0800

    完成练习九，里面可以连续输出多行字符串有点意思

---
+++ ex09.rb
@@ -0,0 +1,14 @@
+# Here's some new strang stuff, remember type is exactly.
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+puts "Here are the days: ", days
+puts "Here are the months: ", months
+
+puts <<PARAGRAPH    # 这个语法有点意思，可以多行输出
+There's something going on here.
+with the three double-quotes.
+we'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+PARAGRAPH
