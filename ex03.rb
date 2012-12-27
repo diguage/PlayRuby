7ec7e97332b6920719eaa0663263a867849504ad
diguage <leejun119@gmail.com>
2012-12-27 12:46:02 +0800

    增加练习三：学习数字计算与判断

---
+++ ex03.rb
@@ -0,0 +1,35 @@
+# Ruby 中支持的数学符号
+#  + 加號
+#  - 減號
+#  / 除號
+#  * 乘號
+#  % 百分比符號
+#  < 小於符號
+#  > 大於符號
+#  <= 小於等於符號
+#  >= 大於等於號
+
+puts "I will now count my chickens."
+
+puts "Hens", 25 + 30 / 6
+puts "Roosters", 100 - 25 * 3 % 4
+
+puts "Now I will count the eggs:"
+
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+puts "Is it true that 3 + 2 < 5 - 7 ?"
+
+puts 3 + 2 < 5 - 7
+
+puts "what is 3 + 2?", 3 + 2
+puts "what is 5 - 7?", 5 - 7
+
+puts "Oh, that's why it's false."
+
+puts "How about some more."
+
+# 学习数字大小的判断
+puts "Is it greater?", 5 > -2
+puts "Is it greater or equal?", 5 >= -2
+puts "Is it less or equal?", 5 <= -2
