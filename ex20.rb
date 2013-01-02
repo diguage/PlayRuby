c2a71fc46b2d1bbdb4620a123457ff516e92e1fd
diguage <leejun119@gmail.com>
2013-01-02 10:00:47 +0800

    瀛︿範浣跨敤+=绗﹀彿

--- ex20.rb
+++ ex20.rb
@@ -10,7 +10,7 @@ end
 
 def print_a_line(line_count, f)
 	puts "#{line_count} #{f.readline()}" # f.readline() 按行阅读吗？
-	f.seek(0, IO::SEEK_SET) # 加上这句，下面三行调用输出的内容一样可以看出，f.readline()是逐行读取，而且每次读取上一次读取的下一行内容。
+	#f.seek(0, IO::SEEK_SET) # 加上这句，下面三行调用输出的内容一样可以看出，f.readline()是逐行读取，而且每次读取上一次读取的下一行内容。
 end
 
 current_file = File.open(input_file)
@@ -29,7 +29,7 @@ puts "Let's print three lines:"
 current_line = 1
 print_a_line(current_line, current_file)
 
-current_line = current_line + 1
+current_line += 1
 print_a_line(current_line, current_file)
 
 current_line = current_line + 1
