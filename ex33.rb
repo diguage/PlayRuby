f96d35b2729cb897709cade575fbd493d3a7ea16
diguage <leejun119@gmail.com>
2013-01-02 18:04:28 +0800

    将原来的功能添加到一个函数中

--- ex33.rb
+++ ex33.rb
@@ -1,17 +1,20 @@
-i = 0
-numbers = []
+def whileLoop (max)
+	i = 0
+	numbers = []
+	while i < max
+		puts "At the top i is #{i}"
+		numbers.push(i)
+	
+		i += 1
+		puts "Numbers now: #{numbers}"
+		puts "At the bottom i is #{i}"
+	end
 
-while i < 6
-	puts "At the top i is #{i}"
-	numbers.push(i)
+	puts "The numbers: "
+	
+	for num in numbers
+		puts num
+	end
+end 
 
-	i += 1
-	puts "Numbers now: #{numbers}"
-	puts "At the bottom i is #{i}"
-end
-
-puts "The numbers: "
-
-for num in numbers
-	puts num
-end
+whileLoop(8) 
